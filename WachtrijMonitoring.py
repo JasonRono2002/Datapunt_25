6d0c22540c98211b9950d8f6ded9f500a934e604
Jason <S1224898@student.windesheim.nl>
2026-05-28 10:21:43 +0200

    Functies toegepast.
    Ik heb functies toegepast om het overzichtelijker/netter te houden. Voor het monitoren van de ingang/uitgang van de attractie.

--- WachtrijMonitoring.py
+++ WachtrijMonitoring.py
@@ -1,39 +1,83 @@
 from machine import Pin, time_pulse_us
 import time
 
+# Pins die verbonden zijn.
 trigIngang = Pin(42, Pin.OUT)
 echoIngang = Pin(41, Pin.IN)
-led = Pin(40, Pin.OUT)
 
-IngangTeller = 0
-aanwezig = False
+trigUitgang = Pin(36, Pin.OUT)
+echoUitgang = Pin(37, Pin.IN)
 
-while True:
-    trigIngang.off()
+ledIngang = Pin(40, Pin.OUT)
+ledUitgang = Pin(38, Pin.OUT)
+
+# Tellers
+in_teller = 0
+uit_teller = 0
+
+# Status
+aanwezig_in = False
+aanwezig_uit = False
+
+# Functie voor het meten van de afstand.
+def meet_afstand(trig, echo):
+    trig.off()
     time.sleep_us(2)
-
-    trigIngang.on()
+    trig.on()
     time.sleep_us(10)
-    trigIngang.off()
+    trig.off()
 
-    duur = time_pulse_us(echoIngang, 1, 30000)
+    duur = time_pulse_us(echo, 1, 30000)
 
     if duur > 0:
-        afstand = round(duur / 58)
+        return round(duur / 58)
     else:
-        afstand = 999
+        return 999
+
+
+# Functie voor de ingang.
+def check_ingang():
+    global in_teller, aanwezig_in
+
+    afstand = meet_afstand(trigIngang, echoIngang)
 
     if afstand < 50:
-        led.on()
+        ledIngang.on()
 
-        if not aanwezig:
-            IngangTeller += 1
-            print("Iemand is binnen")
-            print("Totaal personen binnen:", IngangTeller)
-            aanwezig = True
+        if not aanwezig_in:
+            in_teller += 1
+            aanwezig_in = True
+            print("Iemand is BINNEN gekomen")
+            print("Totaal binnen:", in_teller)
 
     else:
-        led.off()
-        aanwezig = False
+        ledIngang.off()
+        aanwezig_in = False
 
+
+# Functie voor de uitgang
+def check_uitgang():
+    global in_teller, uit_teller, aanwezig_uit
+
+    afstand = meet_afstand(trigUitgang, echoUitgang)
+
+    if afstand < 50:
+        ledUitgang.on()
+
+        if not aanwezig_uit:
+            uit_teller += 1
+            in_teller -= 1
+            aanwezig_uit = True
+
+            print("Iemand is BUITEN gegaan")
+            print("Totaal binnen:", in_teller)
+
+    else:
+        ledUitgang.off()
+        aanwezig_uit = False
+
+
+while True:
+    check_ingang()
+    check_uitgang()
     time.sleep(0.2)
