dfbe98440d7d4944d5509c991fded196c2f113fa
Jason <S1224898@student.windesheim.nl>
2026-05-28 10:33:18 +0200

    Code aangepast.
    Ik heb de code aangepast en korter gemaakt alleen ik moet ze nog in een functie verwerken om het netter te maken.

--- WachtrijMonitoring.py
+++ WachtrijMonitoring.py
@@ -1,25 +1,20 @@
 from machine import Pin, time_pulse_us
 import time
 
-# Pins die verbonden zijn.
-trigIngang = Pin(42, Pin.OUT)
-echoIngang = Pin(41, Pin.IN)
+# Ingang pins -> sensor en led
+trig_in = Pin(42, Pin.OUT)
+echo_in = Pin(41, Pin.IN)
+led_in = Pin(40, Pin.OUT)
 
-trigUitgang = Pin(36, Pin.OUT)
-echoUitgang = Pin(37, Pin.IN)
-
-ledIngang = Pin(40, Pin.OUT)
-ledUitgang = Pin(38, Pin.OUT)
+# Uitgang pins -> sensor en led
+trig_uit = Pin(36, Pin.OUT)
+echo_uit = Pin(37, Pin.IN)
+led_uit = Pin(38, Pin.OUT)
 
 # Tellers
-in_teller = 0
-uit_teller = 0
+totaal_ingang = 0
+totaal_uitgang = 0
 
-# Status
-aanwezig_in = False
-aanwezig_uit = False
-
-# Functie voor het meten van de afstand.
 def meet_afstand(trig, echo):
     trig.off()
     time.sleep_us(2)
@@ -29,55 +24,38 @@ def meet_afstand(trig, echo):
 
     duur = time_pulse_us(echo, 1, 30000)
 
-    if duur > 0:
-        return round(duur / 58)
-    else:
-        return 999
+    if duur < 0:
+        return 999  # geen meting
 
+    return round(duur / 58)
 
-# Functie voor de ingang.
-def check_ingang():
-    global in_teller, aanwezig_in
+try:
+    while True:
+        # Ingang afstand meten
+        afstand_in = meet_afstand(trig_in, echo_in)
 
-    afstand = meet_afstand(trigIngang, echoIngang)
+        if afstand_in < 20:
+            totaal_ingang += 1
+            print(f"Ingang: {totaal_ingang}")
+            led_in.on()
+        else:
+            led_in.off()
 
-    if afstand < 50:
-        ledIngang.on()
+        # Uitgang afstand meten
+        afstand_uit = meet_afstand(trig_uit, echo_uit)
 
-        if not aanwezig_in:
-            in_teller += 1
-            aanwezig_in = True
-            print("Iemand is BINNEN gekomen")
-            print("Totaal binnen:", in_teller)
+        if afstand_uit < 20:
+            totaal_uitgang -= 1
+            print(f"Uitgang: {totaal_uitgang}")
+            led_uit.on()
+        else:
+            led_uit.off()
 
-    else:
-        ledIngang.off()
-        aanwezig_in = False
+        time.sleep(0.2)
 
-
-# Functie voor de uitgang
-def check_uitgang():
-    global in_teller, uit_teller, aanwezig_uit
-
-    afstand = meet_afstand(trigUitgang, echoUitgang)
-
-    if afstand < 50:
-        ledUitgang.on()
-
-        if not aanwezig_uit:
-            uit_teller += 1
-            in_teller -= 1
-            aanwezig_uit = True
-
-            print("Iemand is BUITEN gegaan")
-            print("Totaal binnen:", in_teller)
-
-    else:
-        ledUitgang.off()
-        aanwezig_uit = False
-
-
-while True:
-    check_ingang()
-    check_uitgang()
-    time.sleep(0.2)
+# Als de code stopt dan gaat alles uit.
+finally:
+    led_in.off()
+    led_uit.off()
+    trig_in.off()
+    trig_uit.off()
